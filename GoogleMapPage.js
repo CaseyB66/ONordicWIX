30148edb29c74d0b990da75b7d8f055f874f2da6
CaseyB66 <nekkceb.durfee@gmail.com>
2020-09-25 12:10:24 -0600

    Expanding GoogleMaps functions

--- GoogleMapPage.js
+++ GoogleMapPage.js
@@ -1,3 +1,5 @@
+import wixData from 'wix-data';
+import {getFileInfo} from 'backend/GoogleMap'
 // For full API documentation, including code examples, visit https://wix.to/94BuAAs
 
 $w.onReady(function () {
@@ -6,7 +8,7 @@ $w.onReady(function () {
     $w('#latEdit').max=41.395;
     $w('#lngEdit').max=-111.896
     $w('#lngEdit').min=-111.930;
-    
+    let trailsinfo=getFileInfo("GPXPaths/SkiTrails.gpx")
 	$w("#googleMapHTML").onMessage((event)=>{
 
 		if(event.data.type === 'ready'){
@@ -17,7 +19,8 @@ $w.onReady(function () {
             console.log("onReady got init msg from map: with initCtr "+
                 event.data.value['lat']+","+event.data.value['lng'])
 		}
-	})    
+	})
+    fillTrailRgnDrpDn();
 });
 
 
@@ -36,4 +39,24 @@ export function addLocBtn_click(event) {
     catch (err){
 
     }
+}
+
+function fillTrailRgnDrpDn() {
+	wixData.query("skiTrailsTable")
+		.limit(20)
+		.eq("trailType", "ski")
+		.ascending("viewSort")
+		.find()
+		.then(results => {
+            const rgnsMap = results.items.map(item => item.trailRegion);
+            console.log("fillTrailRgnDrpDn: "+rgnsMap.length+"; type "+typeof(rgnsMap) )
+            const rgns = [new Set(rgnsMap)];
+            console.log("fillTrailRgnDrpDn: "+rgns.length+"; type "+typeof(rgns) )
+			let rgnsOpts = rgns.map(curr => {
+		        return {label:curr, value:curr};})
+            console.log("fillTrailRgnDrpDn: "+rgnsOpts.length+"; type "+typeof(rgnsOpts) )
+			rgnsOpts.push({ label: "All", value: "All" })
+			$w('#trailRgnDrpDn').options = rgnsOpts;
+			$w('#trailRgnDrpDn').selectedIndex = rgnsOpts.length - 1;
+		})
 }
